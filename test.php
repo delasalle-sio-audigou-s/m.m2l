b25671beda410de23903af9388cf1cd49d3640d5
delasalle-sio-audigou-s <delasalle.sio.audigou.sophie@gmail.com>
2016-09-27 15:18:22 +0200

    Sophie ajoute la ligne 8 au fichier test.php

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
 //création d'un fichier par Sophie le 27/09/2016 15:08
-//ajoute d'un ligne par Sophie le 27/09/2016 15:10
+//ajoute d'une ligne par Sophie le 27/09/2016 15:10
 // inclusion de la classe Outils
 include_once ('../modele/Outils.class.php');
 // inclusion des paramètres de l'application
 include_once ('../modele/parametres.localhost.php');
+//ajoute d'une ligne par Sophie le 27/09/2016 15:16
 ?>
