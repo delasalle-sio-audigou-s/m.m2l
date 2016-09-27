7094c6b5c979a69a387aabcc808ef5ef1115dd85
delasalle-sio-bachelier <delasalle.sio.bachelier.v@gmail.com>
2016-09-27 15:14:57 +0200

    modification page test.php

--- test.php
+++ test.php
@@ -1,4 +1,8 @@
 <?php
 //création d'un fichier par Sophie le 27/09/2016 15:08
 //ajoute d'un ligne par Sophie le 27/09/2016 15:10
+// inclusion de la classe Outils
+include_once ('../modele/Outils.class.php');
+// inclusion des paramètres de l'application
+include_once ('../modele/parametres.localhost.php');
 ?>
