18ebe212e426ce8ed59fcc23bddfe6c57e511c93
delasalle-sio-gremy-f <delasalle.sio.gremy.florentin@gmail.com>
2016-11-15 16:59:45 +0100

    FINI

--- controleurs/CtrlAnnulerReservation.php
+++ controleurs/CtrlAnnulerReservation.php
@@ -21,9 +21,9 @@ else
 	
 	// On teste si la réservation existe
 	if (!$dao->existeReservation($idReservation)){
-		$message = "Annulation impossible, la réservation n'existe pas.";
+		$message = "Numéro de réservation inexistant !";
 		$typeMessage = 'avertissement';
-		$themeFooter = $themeNormal;
+		$themeFooter = $themeProbleme;
 		include_once ('vues/VueAnnulerReservation.php');	
 	}
 	else {
@@ -32,17 +32,17 @@ else
 		$laDateReservation = $laReservation->getEnd_time();		
 		
 		if ($laDateReservation <= time()){
-			$message = "Annulation impossible, la réservation est passée.";
+			$message = "Cette réservation est déjà passée !";
 			$typeMessage = 'avertissement';
-			$themeFooter = $themeNormal;
+			$themeFooter = $themeProbleme;
 			include_once ('vues/VueAnnulerReservation.php');
 		}
 		else{
 			// On teste si l'utilisateur est le créateur de la réservation
 			if ( !$dao->estLeCreateur($nomUtilisateur,$idReservation)){
-				$message = "Annulation impossible, vous n'êtes pas le créateur.";
+				$message = "Vous n'êtes pas l'auteur de cette réservation !";
 				$typeMessage = 'avertissement';
-				$themeFooter = $themeNormal;
+				$themeFooter = $themeProbleme;
 				include_once ('vues/VueAnnulerReservation.php');
 			}
 		
@@ -50,12 +50,35 @@ else
 				// Si la réservation existe et a été faite par l'utilisateur elle est annulée
 				$ok = $dao->annulerReservation($idReservation);
 				
-				if ($ok) {
-					$message = 'Réservation annulée.';
-					$typeMessage = 'information';
-					$themeFooter = $themeNormal;
-					include_once ('vues/VueAnnulerReservation.php');
+				if ($ok) {      /////POUR ENVOIE DU MAIL///////
+					//Récupere les informations de l'utilisateur
+					$utilisateur = $dao->getUtilisateur($nomUtilisateur);
+					
+					// inclusion de la classe Outils pour utiliser la méthode envoyer mail
+					include_once ('modele/Outils.class.php');
+					
+					// envoi d'un mail de confirmation de l'enregistrement
+					$sujet = "Annulation de votre Réservation dans le système de réservation de M2L";
+					$contenuMail = "L'administrateur du système de réservations de la M2L vient d'annuler la réservation: " . $idReservation . "\n\n";
+					$envoi = Outils::envoyerMail($utilisateur->getEmail(), $sujet, $contenuMail, $ADR_MAIL_EMETTEUR);
+					
+					if ( ! $envoi ) {
+						// si l'envoi de mail a échoué, réaffichage de la vue avec un message explicatif
+						$message = "Enregistrement effectué.<br>L'envoi du mail à l'utilisateur a rencontré un problème !";
+						$typeMessage = 'avertissement';
+						$themeFooter = $themeProbleme;
+						include_once ('vues/VueAnnulerReservation.php');
+					}
+					else {
+						// tout a fonctionné
+						$message = "Enregistrement effectué.<br>Un mail va être envoyé à l'utilisateur !";
+						$typeMessage = 'information';
+						$themeFooter = $themeNormal;
+						include_once ('vues/VueAnnulerReservation.php');
+					}
+					
 				}
+				
 			}
 		}		
 	}
